3e3db5b642f08bbbba019847c7ab7706a75f9e17
FeloMatta <mattafelo@gmail.com>
2023-07-06 11:56:25 +0200

    Inizializzazione progetto

---
+++ src/main/java/org/lessons/springphotogallery/SpringPhotoGalleryApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.springphotogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPhotoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPhotoGalleryApplication.class, args);
+	}
+
+}
